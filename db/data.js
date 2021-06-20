bc54a92b344f3ce0cd484096acfe8ba04b5704ca
Kyle Moxham <kyle.moxham@built.co.uk>
2021-06-20 21:47:34 +0100

    Add default data and database setup file

---
+++ db/data.js
@@ -0,0 +1,36 @@
+const Users = [
+  {
+    givenName: "Kyle",
+    familyName: "Moxham",
+    email: "email1@email.com",
+    created: new Date().toISOString()
+  },
+  {
+    givenName: "Kyle",
+    familyName: "Moxham",
+    email: "email2@email.com",
+    created: new Date().toISOString()
+  },
+  {
+    givenName: "Kyle",
+    familyName: "Moxham",
+    email: "email3@email.com",
+    created: new Date().toISOString()
+  },
+  {
+    givenName: "Kyle",
+    familyName: "Moxham",
+    email: "email4@email.com",
+    created: new Date().toISOString()
+  },
+  {
+    givenName: "Kyle",
+    familyName: "Moxham",
+    email: "email5@email.com",
+    created: new Date().toISOString()
+  }
+];
+
+module.exports = {
+  Users
+};
---
+++ db/database.js
@@ -0,0 +1,37 @@
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const mongoose = require('mongoose');
+const data = require("./data");
+
+let database = null;
+
+const mongo = new MongoMemoryServer();
+
+async function startDatabase() {
+  const mongoDBURL = await mongo.getUri();
+  const db = await mongoose
+    .connect(
+      mongoDBURL,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }
+    );
+
+  //Seed Database
+  if (!database) {
+    database = db.connection;
+    await database.collection("users").insertMany(data.Users);
+  }
+
+  return database;
+}
+
+async function stopDatabase() {
+  await mongo.stop();
+}
+
+module.exports = {
+  startDatabase,
+  stopDatabase,
+};
